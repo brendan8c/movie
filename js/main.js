8c2d5d8319b4d94087af85683a6e447686836125
Brendan8c <brendan8c@gmail.com>
2023-03-17 18:53:54 +0300

    Added new hoster – Kodik.cc

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ const posterLink = 'https://owlov.ru/img/poster0.webp'; // Cсылка на по
 let keyId; // Глобальная переменная для значения 'keyId' из localStorage
 const bazonLink = 'https://v1674220552.bazon.site/kp/'; // Cсылка для bazon.cc которая будет встроена в iframe
 const pvideocdnLink = 'https://13.annacdn.cc/agAMhXIKvZvI?kp_id='; // Cсылка для videocdn.tv которая будет встроена в iframe
+// const kodikLinkd = 'https://kodik.cc/find-player?kinopoiskID=880618'; // Формируется при получении ответа от сервера в виде объекта
 
 // Слушаем нажатие клавиши Enter
 inputSearch.addEventListener('keydown', event => {
@@ -31,7 +32,7 @@ playerBtn.addEventListener('click', () => {
   inputSearch.value = ''; // Очищаем значение input.value
   return sendRequest(keyId); // Вернуть значение 'keyId' в функцию 'sendRequest()'
 });
-// 4365427  1166515
+// 4365427  1166515  880618
 
 // Отправляю GET запрос на сервер.
 async function sendRequest() {
@@ -67,6 +68,9 @@ async function sendRequest() {
 
     if (data.result === 'bazon') {
       return createPlayerBazon(); // Если фильм есть на bazon, выполняем функцию createPlayerBazon()
+    } else if (typeof data.result === 'object') {
+      log('Response from server:', data);
+      return createPlayerKodik(data.result); // Выполняем функцию createPlayerKodik() и передаем ей URL
     } else if (data.result === 'videocdn') {
       return createPlayerVideocdn(); // Если фильма нет на bazon, выполняем функцию createPlayerVideocdn()
     } else {
@@ -74,6 +78,43 @@ async function sendRequest() {
     }
   } catch (error) {
     console.error(error);
+  }
+}
+
+// Функция создания плеера Kodik с кнопками на сезоны
+// Если ключей только один, то мы берем единственное значение из объекта и устанавливаем его в playerForm.src.
+// Если ключей несколько, то мы создаем кнопки для каждого сезона, устанавливаем им текст и добавляем обработчик клика, который устанавливает соответствующую ссылку на сезон в playerForm.src.
+function createPlayerKodik(data) {
+  const playerBox = document.querySelector('.player-box'); // Найти контейнер плеера
+  const buttonWidth = 100; // Ширина кнопки
+  const distanceBetweenButtons = 10; // Расстояние между кнопками
+
+  // Если есть только одна ссылка на сезон, то загрузить этот сезон
+  if (Object.keys(data).length === 1) {
+    const seasonUrl = Object.values(data)[0];
+    playerForm.src = seasonUrl;
+  } else {
+    const seasonLinks = Object.entries(data); // Преобразование объекта ссылок на сезоны в массив пар [ключ, значение]
+    let leftPosition = 10; // Переменная для расчета позиции по горизонтали
+    for (let i = 0; i < seasonLinks.length; i++) {
+      const seasonNumber = seasonLinks[i][0]; // Номер сезона
+      const seasonUrl = seasonLinks[i][1]; // Ссылка на сезон
+      const button = document.createElement('button'); // Создание кнопки
+      button.innerText = `Сезон ${seasonNumber}`; // Установка текста на кнопке
+      button.classList.add('seasonBtn'); // Добавление класса на кнопку
+      button.style.position = 'absolute'; // Установка позиции кнопки
+      button.style.top = `12px`; // Установка позиции кнопки
+      button.style.left = `${leftPosition}px`; // Установка позиции кнопки
+      button.style.width = `${buttonWidth}px`; // Установка ширины кнопки
+      button.addEventListener('click', () => {
+        // Добавление обработчика событий на кнопку
+        playerForm.src = seasonUrl; // Установка ссылки на сезон
+        const buttons = document.querySelectorAll('.seasonBtn'); // Поиск всех кнопок
+        buttons.forEach(button => button.remove()); // Удаление всех кнопок
+      });
+      playerBox.appendChild(button); // Добавление кнопки в контейнер плеера
+      leftPosition += buttonWidth + distanceBetweenButtons; // Расчет позиции следующей кнопки
+    }
   }
 }
 
